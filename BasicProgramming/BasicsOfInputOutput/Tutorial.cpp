aa07a87bc09fdb360e87015f594859d2ed5bf72a
Aparna Chinya Ramachandra <apchin@microsoft.com>
2016-10-04 09:51:49 +0530

    Basicprogramming first commit

---
+++ BasicProgramming/BasicsOfInputOutput/Tutorial.cpp
@@ -0,0 +1,52 @@
+/*
+You have been given a String S consisting of uppercase and lowercase English alphabets. You need to change the case of each alphabet in this String. That is, all the uppercase letters should be converted to lowercase and all the lowercase letters should be converted to uppercase. You need to then print the resultant String to output.
+Input Format
+The first and only line of input contains the String S
+
+Output Format
+Print the resultant String on a single line. 
+Constraints
+1≤|S|≤100
+
+where 
+|S| denotes the length of string S
+
+SAMPLE INPUT  
+abcdE
+
+SAMPLE OUTPUT
+ABCDe
+
+Ref: https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/modify-the-string/
+
+*/
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+
+int main()
+{
+	char input[100];
+
+	cin >> input;
+
+
+	char x = strlen(input);
+
+	for (int b = 0; b < x; b++)
+	{
+		input[b] = (islower(input[b])) ? toupper(input[b]) : tolower(input[b]);
+		
+	}
+
+	cout << input;
+	
+	
+
+	return 0;
+}
+
+
+
